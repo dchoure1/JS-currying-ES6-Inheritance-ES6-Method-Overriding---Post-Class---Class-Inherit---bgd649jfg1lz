d0e9ac315e3a39bd8383e05a42c30bf296bf3110
dchoure1 <dchoure1@gmail.com>
2022-07-07 14:16:13 +0530

    second

--- src/components/circle.js
+++ src/components/circle.js
@@ -2,5 +2,10 @@
 
 // declare class
 
-
+var Shape = require("./shape");
+// declare class
+class Circle extends Shape{
+   constructor(){super();this.calculateArea=()=>{}}
+};
+module.exports=Circle;
 // export class using module.exports
